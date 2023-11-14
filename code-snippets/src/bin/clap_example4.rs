a0edbc50842305904ead0cf1b0c4a13f7eecaf90
akkuldn <akkul.dn@qxf2.com>
2023-11-14 14:34:22 +0530

    Added example to demonstrate subcommand functionality in clap

--- code-snippets/src/bin/clap_example4.rs
+++ code-snippets/src/bin/clap_example4.rs
@@ -1,50 +1,75 @@
 /*
-    A CLI application for temperature conversion
-*/
+This is a simple application for Git operations that demonstrates usage of subcommands in clap.
+It supports the following subcommands:
 
-use clap::{Command, Arg, ArgGroup};
+ - `clone`: Clone a repository.
+    - Options:
+        - `-n`, `--name`: Name of the repository to clone.
+
+ - `push`: Push changes to a repository.
+    - Options:
+        - `-b`, `--branch`: Branch to push changes to.
+
+ - `commit`: Commit changes to a repository.
+    - Options:
+        - `-m`, `--message`: Commit message.
+*/
+use clap::{Command, Arg};
 
 fn main() {
-    // Build the CLI by defining the configuration using builder pattern
-    let cmd = Command::new("TemperatureConverter")
-        .version("1.0")
-        .about("Convert temperatures between Celsius and Fahrenheit")
-        .arg(
-            Arg::new("temperature")
-                .short('t')
-                .long("temperature")
+    let app_m = Command::new("MyGitApp")
+        .subcommand(Command::new("clone").about("Clone a repository").arg(
+            Arg::new("name")
+                .short('n')
+                .long("name")
                 .num_args(1)
-                .value_parser(clap::value_parser!(f64))
-                .required(true)
-        )
-        .arg(
-            Arg::new("celsius")
-                .short('c')
-                .long("celsius")
-                .num_args(0)
-        )
-        .arg(
-            Arg::new("fahrenheit")
-                .short('f')
-                .long("fahrenheit")
-                .num_args(0)
-        )
-        .group(
-            ArgGroup::new("conversion")
-                .args(["celsius", "fahrenheit"])
-                .required(true)
-        );
+        ))
+        .subcommand(Command::new("push").about("Push changes to a repository").arg(
+            Arg::new("branch")
+                .short('b')
+                .long("branch")
+                .num_args(1)
+        ))
+        .subcommand(Command::new("commit").about("Commit changes to a repository").arg(
+            Arg::new("message")
+                .short('m')
+                .long("message")
+                .num_args(1)
+        ))
+        .get_matches();
+    
 
-    // Runtime argument parsing
-    let matches = cmd.get_matches();
-    let temperature = matches.get_one("temperature").unwrap();
-    let to_celsius = matches.get_one("celsius").unwrap();
-
-    if *to_celsius {
-        let celsius = (temperature - 32.0) * 5.0 / 9.0;
-        println!("{} Fahrenheit is {} Celsius", temperature, celsius);
-    } else {
-        let fahrenheit = (temperature * 9.0 / 5.0) + 32.0;
-        println!("{} Celsius is {} Fahrenheit", temperature, fahrenheit);
+    match app_m.subcommand() {
+        Some(("clone", sub_m)) => {
+            // Handle clone subcommand
+            if let Some(name) = sub_m.get_one::<String>("name") {           
+                println!("Cloning repository with name: {}", name);  
+            } 
+            else {
+                println!("Please provide name of repositor to clone");
+            }
+        }
+        Some(("push", sub_m)) => {
+            // Handle push subcommand
+            if let Some(branch) = sub_m.get_one::<String>("branch") {           
+                println!("Pushing changes to branch: {}", branch);  
+            } 
+            else {
+                println!("Please provide the branch to push");
+            }
+        }
+        Some(("commit", sub_m)) => {
+            // Handle commit subcommand
+            if let Some(message) = sub_m.get_one::<String>("message") {           
+                println!("Commiting changes with message: {}", message);  
+            } 
+            else {
+                println!("Please provide a commit message");
+            }
+        }
+        _ => {
+            // Either no subcommand or invalid subcommand
+            println!("Invalid subcommand or no subcommand provided");
+        }
     }
 }
