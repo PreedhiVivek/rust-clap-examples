534e84003e2a4cc3b31fbe3798545a0fbe218f26
PreedhiVivek <preedhi.vivek@qxf2.com>
2023-11-17 11:53:55 +0530

    Added help for subcommands args

--- code-snippets/src/bin/clap_example4.rs
+++ code-snippets/src/bin/clap_example4.rs
@@ -20,49 +20,52 @@ fn main() {
     let app_m = Command::new("MyGitApp")
         .subcommand(Command::new("clone").about("Clone a repository").arg(
             Arg::new("name")
+                .help("repo name")
                 .short('n')
                 .long("name")
                 .num_args(1)
         ))
         .subcommand(Command::new("push").about("Push changes to a repository").arg(
             Arg::new("branch")
+                .help("branch name")
                 .short('b')
                 .long("branch")
                 .num_args(1)
         ))
         .subcommand(Command::new("commit").about("Commit changes to a repository").arg(
             Arg::new("message")
+                .help("commit message")
                 .short('m')
                 .long("message")
                 .num_args(1)
         ))
         .get_matches();
-    
+
 
     match app_m.subcommand() {
         Some(("clone", sub_m)) => {
             // Handle clone subcommand
-            if let Some(name) = sub_m.get_one::<String>("name") {           
-                println!("Cloning repository with name: {}", name);  
-            } 
+            if let Some(name) = sub_m.get_one::<String>("name") {
+                println!("Cloning repository with name: {}", name);
+            }
             else {
                 println!("Please provide name of repositor to clone");
             }
         }
         Some(("push", sub_m)) => {
             // Handle push subcommand
-            if let Some(branch) = sub_m.get_one::<String>("branch") {           
-                println!("Pushing changes to branch: {}", branch);  
-            } 
+            if let Some(branch) = sub_m.get_one::<String>("branch") {
+                println!("Pushing changes to branch: {}", branch);
+            }
             else {
                 println!("Please provide the branch to push");
             }
         }
         Some(("commit", sub_m)) => {
             // Handle commit subcommand
-            if let Some(message) = sub_m.get_one::<String>("message") {           
-                println!("Commiting changes with message: {}", message);  
-            } 
+            if let Some(message) = sub_m.get_one::<String>("message") {
+                println!("Commiting changes with message: {}", message);
+            }
             else {
                 println!("Please provide a commit message");
             }
